80325b0641c7db53c6aca3c6c56f52e95a595603
micah johnson <micah.johnson150@gmail.com>
2022-02-21 07:38:27 -0700

    Broad tests for generating single plots of each method incorporated at the moment

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -1,20 +1,40 @@
 from snowplot.profiles import *
 from . import data_dir
 import pytest
-from os.path import join
+from os.path import join, dirname, isfile
+from inicheck.tools import get_user_config
+
 
 class TestHandHardnessProfile():
-    @pytest.fixture()
-    def profile(self):
-        filename = join(data_dir, 'hand_hardness.txt')
-        return HandHardnessProfile(filename=filename, plot_id=0)
+    section = 'hand_hardness'
 
-    @pytest.mark.parametrize("letter_value, numeric", [
-        ('F-', 1),
-        ('F+', 3),
-        ('I', 16)
+    @pytest.fixture
+    def config(self, cfg_dict):
+        s = f'[{self.section}]\n'
+        for k,v in cfg_dict.items():
+            s += f'{k}: {v}\n'
+        f = join(dirname(__file__), 'config.ini')
+        s += '[output]\n'
+        with open(f, mode='w+') as fp:
+            fp.write(s)
+        ucfg = get_user_config(f,  modules=['snowplot'])
+        yield ucfg.cfg[self.section]
+        if isfile(f):
+            os.remove(f)
+
+    @pytest.fixture()
+    def profile(self, config):
+
+        filename = join(data_dir, 'hand_hardness.txt')
+        config['filename'] = filename
+        return HandHardnessProfile(**config)
+
+    @pytest.mark.parametrize("cfg_dict, letter_value, numeric", [
+        ({}, 'F-', 1),
+        ({}, 'F+', 3),
+        # ('I', 16)
     ])
-    def test_attribute_scale(self, profile, letter_value, numeric):
+    def test_attribute_scale(self, profile, cfg_dict, letter_value, numeric):
         """
         Test that the scale is being set correctly
         """
---
+++ tests/test_snowplot.py
@@ -0,0 +1,74 @@
+import shutil
+
+from snowplot.snowplot import make_vertical_plot
+from os.path import isfile, dirname, join, isdir
+from inicheck.tools import get_user_config
+from inicheck.output import generate_config
+import shutil
+import os
+import pytest
+from matplotlib.testing.compare import compare_images
+
+class ConfiguredPlotBase:
+    # Section to write to
+    section = None
+    gold_fig = None
+
+    @pytest.fixture(scope='function')
+    def config(self, cfg_dict):
+        s = f'[{self.section}]\n'
+        for k, v in cfg_dict.items():
+            s += f'{k}: {v}\n'
+        f = join(dirname(__file__), 'config.ini')
+        s += '[output]\n'
+        s += 'show_plot: False\n'
+        s += 'dpi: 50\n'
+        s += 'filename: figure.png\n'
+
+        with open(f, mode='w+') as fp:
+            fp.write(s)
+        # Populate the config
+        ucfg = get_user_config(f, modules=['snowplot'])
+        # Write out the new config again
+        generate_config(ucfg, f)
+        yield f
+
+        if isfile(f):
+            os.remove(f)
+
+    @pytest.fixture(scope='function')
+    def figure(self, config, cfg_dict):
+        make_vertical_plot(config)
+
+    @pytest.fixture(scope='function')
+    def gold(self, gold_dir):
+        yield join(gold_dir, self.gold_fig)
+
+    @pytest.fixture(scope='function')
+    def output(self, output_dir):
+        yield join(output_dir, 'figure.png')
+        #
+        # if isdir(output_dir):
+        #     shutil.rmtree(output_dir)
+
+    def test_figure(self, figure, output, gold):
+        assert compare_images(output, gold, 1e-6) is None
+
+
+@pytest.mark.parametrize('cfg_dict', [{'filename': './data/hand_hardness.txt'}])
+class TestHandHardnessPlot(ConfiguredPlotBase):
+    section = 'hand_hardness'
+    gold_fig = 'single_hand_hardness.png'
+
+
+@pytest.mark.parametrize('cfg_dict', [{'filename': './data/lyte_profile.csv',
+                                       'calibration_coefficients': '-1.46799e-06, 0.01441, -50.765, 64700.4'}])
+class TestLyteProbePlot(ConfiguredPlotBase):
+    section = 'lyte_probe'
+    gold_fig = 'single_lyte_profile.png'
+
+
+@pytest.mark.parametrize('cfg_dict', [{'filename': './data/smp.pnt'}])
+class TestSMPPlot(ConfiguredPlotBase):
+    section = 'snow_micropen'
+    gold_fig = 'single_smp_profile.png'
